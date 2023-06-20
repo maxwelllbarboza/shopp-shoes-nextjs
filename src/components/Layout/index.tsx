bc245d130d95db3dd456bc1455413bdb64dca596
Maxwell <Maxwellbarboza@hotmail.com>
2023-06-20 10:51:10 -0300

    Add component Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import Footer from '../Footer';
+import Header from '../Header';
+import styles from './Layout.module.scss';
+
+interface IProps {
+    children: JSX.Element
+}
+
+function Layout({ children }:IProps) {
+    return(
+       <main className={styles.container}>
+        <Header/>
+        <div>{children}</div>
+        <Footer/>
+       </main>
+        
+    )
+}
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
+
+import Layout from '@/components/Layout';
 import styles from '@/styles/Home.module.scss';
 
 export default function Home() {  
  
-  return ( 
-    <div>
-      <Header></Header>
-      <div className= {styles['container-main']} >
-
-      </div>
-
-      <Footer/>
-    </div>     
-      
+  return (
+    <Layout>
+      <div className={styles["container-main"]}></div>
+    </Layout>           
   )
 }
 
